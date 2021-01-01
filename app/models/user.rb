ad4f56852d4343d705eb7408f3a1ba11b7b148dc
Sophie <sophiem@uoguelph.ca>
2021-01-01 18:43:46 -0500

    Added new controller for sessions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
-    
+
+    has_secure_password
     has_many_attached :image
     validates :first_name, :last_name, :email, :password, presence: { message: "is missing."}
     validates :email, uniqueness: { message: ->(object, data) do
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
 
-resources :users
+resources :users, only: [:new, :create]
+
+get 'login', to: 'sessions#new'
+
+post 'login', to: 'sessions#create'
+
+get 'welcome', to: 'sessions#welcome'
+
+get 'authorized', to: 'sessions#page_requires_login'
+
 # about page route
 get 'home/about'
 
